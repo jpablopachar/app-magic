7493a000a776bdc9f74d40c5df27023265ac9809
jpablopachar <jpablopachar1993@gmail.com>
2023-10-14 01:00:57 -0500

    Se crea classlib

---
+++ utility/DS.cs
@@ -0,0 +1,15 @@
+namespace utility
+{
+    public static class DS
+    {
+        public enum API_TYPE
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+        public static string SessionToken = "JWToken";
+    }
+}
